d42b48ba05a389e9f3ae89c931c0d85c9c54340c
Arun Raj <aruunraj@gmail.com>
2022-07-15 18:50:50 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,62 +8,10 @@ export default function Footer({ onClose, isVisible }) {
       <div className="inner split">
         <div>
           <section>
-            <h2>More about me</h2>
+            <h2>Event Dates</h2>
             <p>
-              Part time Open source contributor. Full time Web Developer. Loves
-              to travel and shit.
+              
             </p>
-          </section>
-          <section>
-            <h2>Follow me on ...</h2>
-            <ul className="icons">
-              {config.socialLinks.map(social => {
-                const { icon, name, url } = social;
-                return (
-                  <li key={url}>
-                    <a href={url} className={`icon ${icon}`}>
-                      <span className="label">{name}</span>
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </section>
-        </div>
-        <div>
-          <section>
-            <h2>Get in touch</h2>
-            <form method="post" action="#">
-              <div className="fields">
-                <div className="field half">
-                  <input type="text" name="name" id="name" placeholder="Name" />
-                </div>
-                <div className="field half">
-                  <input
-                    type="text"
-                    name="email"
-                    id="email"
-                    placeholder="Email"
-                  />
-                </div>
-                <div className="field">
-                  <textarea
-                    name="message"
-                    id="message"
-                    rows="4"
-                    placeholder="Message"
-                  />
-                </div>
-              </div>
-              <ul className="actions">
-                <li>
-                  <input type="submit" value="Send" className="primary" />
-                </li>
-                <li>
-                  <input type="reset" value="Reset" />
-                </li>
-              </ul>
-            </form>
           </section>
         </div>
       </div>
